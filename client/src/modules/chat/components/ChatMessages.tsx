ba0f0f6d14bd9ec190a0399e1c9165b1bb499724
Jacobo Jaramillo <Extern.Jacobo.Molina@kuka.com>
2022-03-11 00:11:18 -0500

    fix resizing

--- client/src/modules/chat/components/ChatMessages.tsx
+++ client/src/modules/chat/components/ChatMessages.tsx
@@ -7,19 +7,18 @@ import useWindowDimensions from '../../../hooks/useWindowDimensions';
 
 const MIN_HEIGHT = 500;
 const MIN_WIDTH = 500;
+const MAIN_PADDING = 40;
+const FOOTER_HEIGHT = 38;
+const NAVBAR_HEIGHT = 46;
 
 const ChatMessages: React.FC = () => {
-	const {height: maxHeight, width: maxWidth} = useWindowDimensions();
+	const {height: containerHeight, width: containerWidth} = useWindowDimensions();
 	const [width, setWidth] = useState(MIN_WIDTH);
 	const [height, setHeight] = useState(MIN_HEIGHT);
 
 	const handleResize = ({size}: ResizeCallbackData) => {
-		if (size.width >= MIN_WIDTH) {
-			setWidth(size.width);
-		}
-		if (size.height >= MIN_HEIGHT) {
-			setHeight(size.height);
-		}
+		setWidth(size.width);
+		setHeight(size.height);
 	};
 
 	return (
@@ -27,11 +26,12 @@ const ChatMessages: React.FC = () => {
 			height={height}
 			className="relative"
 			width={width}
-			maxConstraints={[maxWidth, maxHeight]}
+			maxConstraints={[containerWidth - MAIN_PADDING, containerHeight - MAIN_PADDING - FOOTER_HEIGHT - NAVBAR_HEIGHT]}
+			minConstraints={[MIN_WIDTH, MIN_HEIGHT]}
 			onResize={(_, data) => handleResize(data)}
 			handle={<span className="resize-handle" />}
 			// handle={<RadiusUpleftOutlined className="absolute right-0 bottom-0 cursor-nwse-resize" />}
-			>
+		>
 			<Card className="w-full h-full">test</Card>
 		</ResizableBox>
 	);
